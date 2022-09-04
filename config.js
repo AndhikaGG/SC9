df8974c038947f743010d66eb76d0018df6c614f
Andhika <78576587+AndhikaGG@users.noreply.github.com>
2022-09-04 11:35:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['6281358929103']
+global.premium = ['6281358929103']
+global.packname = 'ND_Bot'
+global.author = 'By ND_Bot ( wa.me/6281358929103)'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
